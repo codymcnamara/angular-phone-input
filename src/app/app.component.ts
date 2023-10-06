11f1af1eb9bdcab4ed18799bc99ea7ceabfb01c9
codymcnamara <codyjamesmcnamara@gmail.com>
2023-10-05 20:36:51 -0700

    set up formatted phone input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-phone-input';
+
+  phone = new FormControl('');
+
+  handleChange(event: any){
+    if (typeof this.phone.value === 'string' ){
+      let justNumString = '';
+      if ( this.phone.value.match(/\d+/g) !== null ) {
+        let justNumsArr = this.phone.value.match(/\d+/g);
+        if(Array.isArray(justNumsArr)){
+          justNumString = justNumsArr.join('');
+        }
+      }
+
+      if(justNumString.length >= 3 && justNumString.length < 6){
+        this.phone.setValue('(' + justNumString.slice(0,3) + ') ' + justNumString.slice(3,justNumString.length));
+      } else if (justNumString.length >= 6){
+        this.phone.setValue('(' + justNumString.slice(0,3) + ') ' + justNumString.slice(3, 6) + '-' + justNumString.slice(6,10));
+      }
+
+    }
+
+  }
 }
